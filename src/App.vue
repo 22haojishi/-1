12ca91bae56afd5f062913f98aa27a010cbc3666
Administrator <1103454741@qq.com>
2018-07-06 09:22:10 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -33,11 +33,11 @@ a {
 a, button {
     cursor: pointer;
 }
-#app {
+/* #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
+} */
 </style>
